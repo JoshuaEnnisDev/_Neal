108b886f12bd588be371251169693f381dfe4183
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-10-01 11:23:59 -0500

    commit

--- CS50/week 0/faces.py
+++ CS50/week 0/faces.py
@@ -1,4 +1,5 @@
 emoji_sentence = input().replace(":)", "🙂").replace(":(", "🙁")
 # emoji_sentence = emoji_sentence.replace(":)", "🙂")
 # emoji_sentence = emoji_sentence.replace(":(", "🙁")
+# emoji_sentence = emoji_sentence.replace(":(", "🙁")
 print(emoji_sentence)
